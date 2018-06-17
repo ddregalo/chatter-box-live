8adfbeb2b7fef7889f7c21712fdb2381ff2c407e
danielcampos <ddregalo@gmail.com>
2018-06-17 12:29:08 +0100

    Create port constant variable

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
-
+const PORT = 3000;
 app.use(express.static(__dirname + "/lib/views/"))
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`)
+})
