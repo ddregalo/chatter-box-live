4bdee811c6faa180660c2458bb43e8bfba1d08b9
danielcampos <ddregalo@gmail.com>
2018-06-17 23:16:34 +0100

    Implement create message and display

--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 const PORT = 3000;
 
 app.use(express.static(__dirname + "/views/"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 var messages = [
   {name: 'Bhav', message: 'Message 1'},
   {name: 'Katie', message: 'Message 2'}
 ]
 
-app.get("/allMessages", (req, res, err) => {
+app.get("/allMessages", (req, res) => {
   res.send(messages)
+})
+
+app.post("/allMessages", (req, res) => {
+  console.log(req.body)
+  messages.push(req.body)
+  res.sendStatus(200)
 })
 
 app.listen(PORT, () => {
