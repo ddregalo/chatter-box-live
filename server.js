bd650dbbddd8c8c9c8bee2877d1c4ab0da3099ab
danielcampos <ddregalo@gmail.com>
2018-06-17 17:34:20 +0100

    Change lib to src folder and implement add msg form to index

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var app = express()
+var path = require('path');
 const PORT = 3000;
-app.use(express.static(__dirname + "/lib/views/"))
+
+app.use(express.static(__dirname + "/src/views/"))
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`)
 })
